cdea3cdfb5f7cbfa5bc7beda7849391c04af8c66
Divine <emmanueldivine246@gmail.com>
2022-10-14 15:29:20 +0000

    twitter reg. home clone

--- sectionsPage/script.js
+++ sectionsPage/script.js
@@ -1,7 +1,17 @@
 var icons = document.querySelectorAll(".row-one i")
+// var rowones = document.querySelectorAll(".row-one");
+// var removeClass = document.querySelector("#removeClass");
+
+// removeClass.onclick = function() {
+//     var seconds = document.querySelectorAll("#removeClass .full-card__row-two");
+//     seconds.forEach(function(second) {
+//         second.classList.remove("open")
+//     })
+// }
 
 icons.forEach(function(icon) {
     icon.onclick = function() {
-        this.classList.toggle("open")
+        // icon.classList.remove("open")
+        this.classList.toggle("open");
     }
 })
